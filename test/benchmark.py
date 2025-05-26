1ff95b5a5dc96acdccf712c17db343ee95989a97
apalrd <adventure@apalrd.net>
2025-05-26 00:14:16 +0000

    make optimizations

--- test/benchmark.py
+++ test/benchmark.py
@@ -72,9 +72,7 @@ def run_iperf3_tcp_test(server, port=5201, duration=10,reverse=False,unlimited=T
 
 #test.debug = True
 test.timeout = 0.1
-#test.use_valgrind = True
-test.tayga_conf.default()
-test.tayga_conf.dynamic_pool = None
+test.use_valgrind = True
 test.setup()
 
 # Start iperf3 server
