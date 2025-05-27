384f6e09561da6d8943df04ed0fc1c8164c3336a
apalrd <adventure@apalrd.net>
2025-05-27 06:47:22 +0000

    Work on nat64 docs

---
+++ docs/nat64/nat64-static.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+#Enable IP forwarding of IPv4 and IPv6
+#echo 1 > /proc/sys/net/ipv4/conf/all/forwarding
+#echo 1 > /proc/sys/net/ipv6/conf/all/forwarding
+
+# Masquerade using ip6tables
+# for each public IP
+for pub in $(seq 0 255)
+do
+    # for each host within this public IP
+    for sub in $(seq 0 15)
+    do
+        echo Configuring client pub $pub sub $sub
+        echo IPv6 range is $(printf '2001:db8:1%02x%01x::/48' $pub $sub)
+        echo Translation range is $(printf 'fd64::%x' $pub)
+        echo Port range is $(printf '%d:%d' $((1024+4000*$sub)) $((5023+4000*$sub)))
+        ip6tables -t nat -A POSTROUTING -s $(printf '2001:db8:1%02x%01x::/48' $pub $sub) -o nat64 -J MASQUERADE --to-source $(printf 'fd64::%x' $pub) --to-ports  $(printf '%d-%d' $((1024+4000*$sub)) $((5023+4000*$sub)))
+    done
+done
+
+return
+
+# Bring up tun interface w/ tayga
+tayga -c nat64-static.conf --mktun
+
+# Bring up interface
+ip link set dev nat64 up
+# Add routes to the tun interface (distribute these via your routing protocol)
+ip route add 203.0.113.0/24 dev nat64
+ip route add 192.0.2.0/32 dev nat64
+# Add IPv6 address (implicitly adds /64 route) (distribute this via your routing protocol)
+ip addr add 2001:db8:0:6464::/64 dev nat64
+# Add routes to translation network
+ip route add fd64::/64 dev nat64
+# Add pref64 route (distribute this via your routing protocol))
+ip route add 64:ff9b::/96 dev nat64
+
+# Start Tayga
+tayga -c nat64-static.conf 
