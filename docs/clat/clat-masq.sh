db6faeacaeb11fea75ee1f1c6d503ba06489d38d
apalrd <adventure@apalrd.net>
2025-05-27 05:36:33 +0000

    clat docs

--- docs/clat/clat-masq.sh
+++ docs/clat/clat-masq.sh
@@ -1,9 +1,6 @@
 #Enable IP forwarding of IPv6 (not required for IPv4)
 echo 1 > /proc/sys/net/ipv6/conf/all/forwarding
 
-#Enable Accept RA (=2 due to forwarding) on eth0 for SLAAC address assignment
-echo 2 > /proc/sys/net/ipv6/conf/eth0/accept_ra
-
 # Masquerade using ip6tables
 # Note: IPv6 Masquerade is a dedicated kernel config option
 # It is often compiled as a module, or left out entirely
@@ -15,8 +12,8 @@ tayga -c clat.conf --mktun
 
 # Add IPv4 IP (implicitly adds /29 route)
 ip addr add 192.0.0.1/29 dev clat
-# Add IPv4 default route
-ip route add default via 192.0.0.2 dev clat
+# Add IPv4 default route w/ mtu 1280 to prevent fragmentation
+ip route add default via 192.0.0.2 dev clat mtu 1260
 # Add IPv6 address (implicitly adds /64 route)
 ip addr add fd64::/64 dev clat
 
--- docs/clat/clat-proxynd.sh
+++ docs/clat/clat-proxynd.sh
@@ -16,8 +16,8 @@ tayga -c clat-proxynd.conf --mktun
 
 # Add IPv4 IP (implicitly adds /29 route)
 ip addr add 192.0.0.1/29 dev clat
-# Add IPv4 default route
-ip route add default via 192.0.0.2 dev clat
+# Add IPv4 default route with mtu 1280 to prevent fragmentation
+ip route add default via 192.0.0.2 dev clat mtu 1260
 # Add IPv6 route (/127 adds both ::7 and ::6)
 ip route add 2001:db8:feed::7/127 dev clat 
 
