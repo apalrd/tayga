apalrd/tayga

commit 28030e95e68321e6db5fa4863f9f0188be29b2a2

Preliminary Github Actions (#86)
* more yaml work

Makefile
@@ -1,76 +1,36 @@
-# Simple Makefile generated based on makefile.am
-
+# Default compiler flags
 CC := gcc
 CFLAGS := -Wall -O2
 LDFLAGS := -flto=auto
 SOURCES := nat64.c addrmap.c dynamic.c tayga.c conffile.c
-TARGET := tayga
-TARGET-COV := $(TARGET)-cov
 
-all: $(TARGET)
-cov: $(TARGET-COV)
-
-# Version generation
-version.h: .git/index
+# Compile Tayga
+.PHONY: all
+all:
+ifndef $(RELEASE)
+#Generate version.h from git
 	@echo "#define TAYGA_VERSION \"$(shell git describe --tags --dirty)\"" > $@
 	@echo "#define TAYGA_BRANCH \"$(shell git describe --all --dirty)\"" >> $@
 	@echo "#define TAYGA_COMMIT \"$(shell git rev-parse HEAD)\"" >> $@
-
-# Dependency generation
-tayga.d: $(SOURCES) version.h Makefile
-	$(CC) $(CFLAGS) -MM $(SOURCES) -MT tayga $< > $@
-
--include tayga.d
-
-# Build targets
-$(TARGET): $(SOURCES) tayga.d
+endif
 	$(CC) $(CFLAGS) -o $@ $(SOURCES) $(LDFLAGS)
 
-static: $(SOURCES) tayga.d
-	$(CC) $(CFLAGS) -o $(TARGET) $(SOURCES) $(LDFLAGS) -static
-
-$(TARGET-COV): $(TARGET)
-	$(CC) $(LDFLAGS) -o $@ $(SOURCES) -coverage -fcondition-coverage -DCOVERAGE_TESTING
-
-cov-report:
-	gcov -a -g -f *.gcno
-
-# Container images
-container: tayga-clat.tar tayga-nat64.tar tayga.tar
-.PHONY: container
-
-tayga.tar: tayga launch.sh
-	$(RM) $@
-	podman manifest create tayga
-	podman build --all-platforms . --manifest tayga
-	podman manifest push --all tayga ghcr.io/apalrd/tayga:latest
-	podman save -o $@ tayga
-	podman manifest rm tayga
-
-tayga-clat.tar: tayga launch-clat.sh
-	$(RM) $@
-	podman manifest create tayga-clat
-	podman build --all-platforms . --manifest tayga-clat --target final-clat
-	podman manifest push --all tayga-clat ghcr.io/apalrd/tayga-clat:latest
-	podman save -o $@ tayga-clat
-	podman manifest rm tayga-clat
-
-tayga-nat64.tar: tayga launch-nat64.sh
-	$(RM) $@
-	podman manifest create tayga-nat64
-	podman build --all-platforms . --manifest tayga-nat64 --target final-nat64
-	podman manifest push --all tayga-nat64 ghcr.io/apalrd/tayga-nat64:latest
-	podman save -o $@ tayga-nat64
-	podman manifest rm tayga-nat64
+# Compile Tayga (static)
+.PHONY: static
+static:
+ifndef $(RELEASE)
+#Generate version.h from git
+	@echo "#define TAYGA_VERSION \"$(shell git describe --tags --dirty)\"" > $@
+	@echo "#define TAYGA_BRANCH \"$(shell git describe --all --dirty)\"" >> $@
+	@echo "#define TAYGA_COMMIT \"$(shell git rev-parse HEAD)\"" >> $@
+endif
+	$(CC) $(CFLAGS) -o $@ $(SOURCES) $(LDFLAGS) -static
 
 clean:
-	$(RM) $(TARGET) tayga.d version.h $(TARGET-COV) tayga-nat64.tar tayga-clat.tar
-	$(RM) *.gcda *.gcno *.gcov
+	$(RM) tayga version.h
 
 install: $(TARGET)
 	# TODO
 
 uninstall:
 	# TODO
-
-.PHONY: all clean install uninstall cov-report

.github/workflows/release.yml
@@ -1,32 +1,52 @@
-# This is a basic workflow that is manually triggered
-
-name: Manual workflow
+name: Release
 
 # Controls when the action will run. Workflow runs when manually triggered using the UI
 # or API.
 on:
   workflow_dispatch:
-    # Inputs the workflow accepts.
     inputs:
-      name:
-        # Friendly description to be shown in the UI instead of 'name'
-        description: 'Person to greet'
-        # Default value if no value is explicitly provided
-        default: 'World'
-        # Input has to be provided for the workflow to run
+      ver:
+        description: 'Version tag'
         required: true
-        # The data type of the input
         type: string
 
 # A workflow run is made up of one or more jobs that can run sequentially or in parallel
 jobs:
-  # This workflow contains a single job called "greet"
-  greet:
-    # The type of runner that the job will run on
+  # Create version.h and tag release
+  tag-rel:
+    permissions:
+        contents: write
     runs-on: ubuntu-latest
-
-    # Steps represent a sequence of tasks that will be executed as part of the job
     steps:
-    # Runs a single command using the runners shell
-    - name: Send greeting
-      run: echo "Hello ${{ inputs.name }}"
+      - uses: actions/checkout@v4
+        with:
+            # Fetch all
+            fetch-depth: 0
+      - name: Tag release
+        run: |        
+            echo "#define TAYGA_VERSION \"${{ inputs.ver }}\"" > version.h
+            echo "#define TAYGA_BRANCH \"main\"" >> version.h
+            echo "#define TAYGA_COMMIT \"RELEASE\"" >> version.h
+            git add version.h
+            git config user.name "github-actions[bot]"
+            git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
+            git commit -m "autogenerate release version file"
+            git tag ${{ inputs.ver }}
+            git push origin ${{ inputs.ver }}
+
+  build-amd64:
+    needs: tag-rel
+    runs-on: ubuntu-latest  
+    steps:
+      - uses: actions/checkout@v4
+        with: 
+            ref: ${{ inputs.ver }}
+      - uses: jirutka/setup-alpine@v1
+        with:
+            branch: v3.19
+      - name: Install tools
+        run: apk add gcc make
+        shell: alpine.sh --root {0}
+      - name: run build
+        run: make static RELEASE=${{ inputs.ver }}
+        shell: alpine.sh {0}

.github/workflows/tests.yml
@@ -1,0 +1,33 @@
+# This is a basic workflow that is manually triggered
+
+name: Run Test Suite
+
+# Controls when the action will run. Workflow runs when manually triggered using the UI
+# or API.
+on:
+  workflow_dispatch:
+
+# A workflow run is made up of one or more jobs that can run sequentially or in parallel
+jobs:
+    test:
+        runs-on: ubuntu-latest  
+        steps:
+          - uses: actions/checkout@v4
+          - uses: jirutka/setup-alpine@v1
+            with:
+                branch: v3.19
+          - name: Install tools
+            run: apk add gcc make
+            shell: alpine.sh --root {0}
+          - name: run build
+            run: make
+            shell: alpine.sh {0}
+          - name: install scapy
+            run: sudo pip3 install scapy
+          - name: run test
+            run: sudo test/fullsuite.sh
+          - uses: actions/upload-artifact@v4
+            with:
+                name: results
+                path: test/*.rpt test/*.log
+                if-no-files-found: error

tayga.8
@@ -1,4 +1,4 @@
-.TH TAYGA "8" "Dec 2018" "TAYGA 0.9.2" ""
+.TH TAYGA "8" "Jun 2025" "TAYGA 0.9.4" ""
 
 .SH NAME
 tayga \- stateless NAT64 daemon
@@ -14,13 +14,13 @@ tayga \- stateless NAT64 daemon
 .I [OPTION]...
 
 .SH DESCRIPTION
-TAYGA is a stateless NAT64 daemon for Linux.  Using the in-kernel TUN network
-driver, TAYGA receives IPv4 and IPv6 packets from the host's network stack,
-translates them to the other protocol, and then sends the translated packets
-back to the host using the same TUN interface.
+TAYGA is a stateless NAT64 daemon for Linux and FreeBSD.  Using the in-kernel
+TUN network driver, TAYGA receives IPv4 and IPv6 packets from the host's
+network stack, translates them to the other protocol, and then sends the
+translated packets back to the host using the same TUN interface.
 .P
-Translation is compliant with IETF RFC 6145, and address mapping is
-performed in accordance with RFC 6052 or RFC 7757.  Optionally, TAYGA may be
+Translation is compliant with IETF RFC 7915, and address mapping is
+performed in accordance with RFC 6052 and RFC 7757.  Optionally, TAYGA may be
 configured to dynamically map IPv6 hosts to addresses drawn from a
 configured IPv4 address pool.
 .P
@@ -87,9 +87,10 @@ chroot() to data\-dir (specified in config file)
 Write process ID of daemon to
 .I pidfile
 .SH AUTHOR
-Written by Nathan Lutchansky <lutchann@litech.org>
+Maintained by Andrew Palardy <andrew@apalrd.net>
 .SH COPYRIGHT
 Copyright \(co 2010 Nathan Lutchansky
+Copyright \(co 2025 Andrew Palardy
 .br
 License GPLv2+: GNU GPL version 2 or later
 .br
@@ -99,4 +100,4 @@ There is NO WARRANTY, to the extent permitted by law.
 
 .BR tayga.conf (5)
 .br
-.BR <http://www.litech.org/tayga/>
+.BR <https://github.com/apalrd/tayga/>

test/conffile.py
@@ -1,6 +1,0 @@
-#
-#   part of TAYGA <https://github.com/apalrd/tayga> test suite
-#   Copyright (C) 2025  Andrew Palardy <andrew@apalrd.net>
-# 
-#   test/conffile.py - Test configuration file parsing and validation
-#

test/perf/test-iperf.sh
@@ -1,29 +1,0 @@
-#!/bin/bash
-
-# Setup test
-. test/setup.sh
-
-set -x
-# Start iperf server
-iperf3 -s -p 6464 -D -I /var/run/iperf.pid || exit 1
-# Check if iperf started successfully
-if [ ! -f /var/run/iperf.pid ]; then
-    echo "iperf failed to start"
-    exit 1
-fi
-
-# Run iperf from IPv6 to IPv4
-iperf3 -c $TAYGA_PREFIX$TEST_SYSTEM_IPV4 -p 6464 -t 10 -B $TEST_SYSTEM_IPV6 -b 10m
-
-# Run iperf from IPv4 to IPv6
-iperf3 -c $TAYGA_PREFIX$TEST_SYSTEM_IPV4 -p 6464 -t 10 -B $TEST_SYSTEM_IPV6 -b 10m -R
-
-
-# Stop iperf server
-kill -9 $(cat /var/run/iperf.pid) || exit 1
-# Remove the PID file
-rm -f /var/run/iperf.pid || exit 1
-
-set +x
-# Cleanup test
-. test/cleanup.sh

commit 8aae8527ff11fd2dc0904fc4fda5e3bc2a204426

Makefile: drop the unset/unused TARGET variable
Fixes: 28030e95e683 ("Preliminary Github Actions (#86)")

Makefile
@@ -82,7 +82,7 @@ clean:
 # If systemd is detected, copy systemd service file
 # If systemd is detected and conf does not already exist, copy example conf file
 .PHONY: install install-live
-install: $(TARGET)
+install:
 	-mkdir -p $(DESTDIR)$(sbindir) $(DESTDIR)$(mandir)/man5 $(DESTDIR)$(mandir)/man8
 	$(INSTALL_PROGRAM) tayga $(DESTDIR)$(sbindir)/tayga
 	$(INSTALL_DATA) tayga.8 $(DESTDIR)$(mandir)/man8