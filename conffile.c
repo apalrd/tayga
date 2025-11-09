apalrd/tayga

commit 8578d24f9669232e5a2e35db64fbf8d2bd79de8f

Documentation & Work (#59)
* Work on docs w/ doxygen

* Work on Valgrind in test env

* Routing Header w/ segs remaining

* Fragment / MTU changes

conffile.c
@@ -414,7 +414,6 @@ void read_config(char *conffile)
 	gcfg->max_commit_delay = gcfg->dyn_max_lease / 4;
 	gcfg->hash_bits = 7;
 	gcfg->cache_size = 8192;
-	gcfg->allow_ident_gen = 1;
 	gcfg->ipv6_offlink_mtu = 1280;
 	gcfg->lazy_frag_hdr = 1;
 	INIT_LIST_HEAD(&gcfg->cache_pool);
@@ -477,10 +476,11 @@ void read_config(char *conffile)
 	m4 = list_entry(gcfg->map4_list.next, struct map4, list);
 	m6 = list_entry(gcfg->map6_list.next, struct map6, list);
 
-	if (m4->type == MAP_TYPE_RFC6052 && m6->type == MAP_TYPE_RFC6052 &&
-			!gcfg->allow_ident_gen)
+	if (m4->type == MAP_TYPE_RFC6052 && m6->type == MAP_TYPE_RFC6052) {
+		slog(LOG_DEBUG,"Disabling cache, not required\n");
 		gcfg->cache_size = 0;
-
+	}
+	
 	if (!gcfg->local_addr4.s_addr) {
 		slog(LOG_CRIT, "Error: no ipv4-addr directive found\n");
 		exit(1);

nat64.c
@@ -39,7 +39,7 @@ static uint16_t ip_checksum(void *d, int c)
 	return ~sum;
 }
 
-static uint16_t ones_add(uint16_t a, uint16_t b)
+static inline uint16_t ones_add(uint16_t a, uint16_t b)
 {
 	uint32_t sum = (uint16_t)~a + (uint16_t)~b;
 
@@ -66,20 +66,23 @@ static uint16_t ip6_checksum(struct ip6 *ip6, uint32_t data_len, uint8_t proto)
 
 static uint16_t convert_cksum(struct ip6 *ip6, struct ip4 *ip4)
 {
-	uint32_t sum = 0;
-	uint16_t *p;
-	int i;
+	uint64_t sum = 0;
+	
+	sum += ~ip4->src.s_addr;
+	sum += ~ip4->dest.s_addr;
+	sum += ip6->src.s6_addr32[0];
+	sum += ip6->src.s6_addr32[1];
+	sum += ip6->src.s6_addr32[2];
+	sum += ip6->src.s6_addr32[3];
+	sum += ip6->dest.s6_addr32[0];
+	sum += ip6->dest.s6_addr32[1];
+	sum += ip6->dest.s6_addr32[2];
+	sum += ip6->dest.s6_addr32[3];
 
-	sum += ~ip4->src.s_addr >> 16;
-	sum += ~ip4->src.s_addr & 0xffff;
-	sum += ~ip4->dest.s_addr >> 16;
-	sum += ~ip4->dest.s_addr & 0xffff;
-
-	for (i = 0, p = ip6->src.s6_addr16; i < 16; ++i)
-		sum += *p++;
-
-	while (sum > 0xffff)
-		sum = (sum & 0xffff) + (sum >> 16);
+	/* Fold carry-arounds */
+	if(sum > 0xffffffff) sum = (sum & 0xffffffff) + (sum >> 32);
+	if(sum > 0xffff) sum = (sum & 0xffff) + (sum >> 16);
+	if(sum > 0xffff) sum = (sum & 0xffff) + (sum >> 16);
 
 	return sum;
 }
@@ -475,10 +478,11 @@ static void xlate_4to6_icmp_error(struct pkt *p)
 			if (mtu < 68)
 				mtu = est_mtu(ntohs(p_em.ip4->length));
 			mtu += MTU_ADJ;
+			/* Path MTU > our own MTU */
 			if (mtu > gcfg->mtu)
 				mtu = gcfg->mtu;
-			if (mtu < 1280 && gcfg->allow_ident_gen && orig_dest) {
-				orig_dest->flags |= CACHE_F_GEN_IDENT;
+			/* Set MTU to 1280 to prevent generation of atomic fragments */
+			if (mtu < 1280) {
 				mtu = 1280;
 			}
 			header.icmp.word = htonl(mtu);
@@ -672,18 +676,28 @@ static void xlate_header_6to4(struct pkt *p, struct ip4 *ip4,
 	ip4->ver_ihl = 0x45;
 	ip4->tos = (ntohl(p->ip6->ver_tc_fl) >> 20) & 0xff;
 	ip4->length = htons(sizeof(struct ip4) + payload_length);
+	/* Have an IPv6 fragment header, translate to a v4 fragment */
 	if (p->ip6_frag) {
 		ip4->ident = htons(ntohl(p->ip6_frag->ident) & 0xffff);
 		ip4->flags_offset =
 			htons(ntohs(p->ip6_frag->offset_flags) >> 3);
 		if (p->ip6_frag->offset_flags & htons(IP6_F_MF))
 			ip4->flags_offset |= htons(IP4_F_MF);
-	} else if (dest && (dest->flags & CACHE_F_GEN_IDENT) &&
-			p->header_len + payload_length <= 1280) {
-		ip4->ident = htons(dest->ip4_ident++);
+		/* Always clear DF bit */
+		ip4->flags_offset &= ~htons(IP4_F_DF);
+	/* Smol packets can be fragmented downstream */
+	} else if (p->header_len + payload_length <= 1280) {
+		/* Need to generate a psuedo-random ident value
+		 * A simple counter is not secure enough
+		 * However, it doesn't actually seem to be that random in practice
+		 * ref. https://datatracker.ietf.org/doc/html/rfc7739#appendix-B
+		 * */
+		static uint32_t ident = 0xb00b;
+		if(ident & 0x1) ident ^= 0x6464beef;
+		ident >>= 1;
+		ip4->ident = (ident& 0xffff);
 		ip4->flags_offset = 0;
-		if (dest->ip4_ident == 0)
-			dest->ip4_ident++;
+	/* Packets > 1280 must kick back a Packet Too Big */
 	} else {
 		ip4->ident = 0;
 		ip4->flags_offset = htons(IP4_F_DF);
@@ -796,6 +810,8 @@ static void xlate_6to4_data(struct pkt *p)
 static int parse_ip6(struct pkt *p)
 {
 	int hdr_len;
+	uint8_t seg_left = 0;
+	uint16_t seg_ptr = sizeof(struct ip6);
 
 	p->ip6 = (struct ip6 *)(p->data);
 
@@ -819,6 +835,13 @@ static int parse_ip6(struct pkt *p)
 		hdr_len = (p->data[1] + 1) * 8;
 		if (p->data_len < hdr_len)
 			return -1;
+		/* If it's a routing header, extract segments left 
+		 * We will drop the packet, but need to finish parsing it first
+		 */
+		if(p->data_proto == 43) seg_left = p->data[3];
+		if(!seg_left) seg_ptr += hdr_len;
+
+		/* Extract next header from extension header */
 		p->data_proto = p->data[0];
 		p->data += hdr_len;
 		p->data_len -= hdr_len;
@@ -850,6 +873,17 @@ static int parse_ip6(struct pkt *p)
 		if (p->data_len < sizeof(struct icmp))
 			return -1;
 		p->icmp = (struct icmp *)(p->data);
+	}
+
+	/* IF we got a routing header with segments left
+	 * kick back a Parameter Problem pointing to the seg field
+	 */
+	if(seg_left) {
+		seg_ptr += 4;
+		slog(LOG_DEBUG,"%s:%d:IPv6 Routing Header w/ Segments Left ptr=%d\n", 
+			 __FUNCTION__,__LINE__,seg_ptr);
+		host_send_icmp6_error(4, 0, seg_ptr, p);
+		return -1;
 	}
 
 	return 0;

tayga.c
@@ -17,6 +17,7 @@
  */
 
 #include "tayga.h"
+#include "version.h"
 
 #include <stdarg.h>
 #include <signal.h>
@@ -581,7 +582,7 @@ int main(int argc, char **argv)
 		close(pidfd);
 	}
 
-	slog(LOG_INFO, "starting TAYGA " VERSION "\n");
+	slog(LOG_INFO, "starting TAYGA " TAYGA_VERSION "\n");
 
 	if (gcfg->cache_size) {
 		gcfg->urandom_fd = open("/dev/urandom", O_RDONLY);

tayga.h
@@ -47,8 +47,6 @@
 #error "Could not find headers for platform"
 #endif
 #include "list.h"
-#include "config.h"
-
 //for coverage testing
 static void dummy()
 {
@@ -90,6 +88,9 @@ struct tun_pi {
 
 /* Valid token delimiters in config file and dynamic map file */
 #define DELIM		" \t\r\n"
+
+/// Default configuration path
+#define TAYGA_CONF_PATH "/etc/tayga.conf"
 
 
 /* Protocol structures */
@@ -149,6 +150,7 @@ struct icmp {
 
 /* TAYGA data definitions */
 
+/// Packet structure
 struct pkt {
 	struct ip4 *ip4;
 	struct ip6 *ip6;
@@ -160,6 +162,7 @@ struct pkt {
 	uint32_t header_len; /* inc IP hdr for v4 but excl IP hdr for v6 */
 };
 
+/// Type of mapping in mapping list
 enum {
 	MAP_TYPE_STATIC,
 	MAP_TYPE_RFC6052,
@@ -167,6 +170,7 @@ enum {
 	MAP_TYPE_DYNAMIC_HOST,
 };
 
+/// Mapping entry (IPv4)
 struct map4 {
 	struct in_addr addr;
 	struct in_addr mask;
@@ -175,6 +179,7 @@ struct map4 {
 	struct list_head list;
 };
 
+/// Mapping entry (IPv6)
 struct map6 {
 	struct in6_addr addr;
 	struct in6_addr mask;
@@ -183,6 +188,7 @@ struct map6 {
 	struct list_head list;
 };
 
+/// Mapping entry (Static Maps)
 struct map_static {
 	struct map4 map4;
 	struct map6 map6;
@@ -195,6 +201,7 @@ struct free_addr {
 	struct list_head list;
 };
 
+/// Mapping entry (Dynamic Map)
 struct map_dynamic {
 	struct map4 map4;
 	struct map6 map6;
@@ -204,6 +211,7 @@ struct map_dynamic {
 	struct free_addr free;
 };
 
+/// Mapping entry (Dynamic Pool)
 struct dynamic_pool {
 	struct map4 map4;
 	struct list_head mapped_list;
@@ -212,6 +220,7 @@ struct dynamic_pool {
 	struct free_addr free_head;
 };
 
+/// IP Cache entry
 struct cache_entry {
 	struct in6_addr addr6;
 	struct in_addr addr4;
@@ -223,11 +232,15 @@ struct cache_entry {
 	struct list_head hash6;
 };
 
-#define CACHE_F_SEEN_4TO6	(1<<0)
-#define CACHE_F_SEEN_6TO4	(1<<1)
-#define CACHE_F_GEN_IDENT	(1<<2)
-#define CACHE_F_REP_AGEOUT	(1<<3)
+/// Cache flag bits
+enum {
+	CACHE_F_SEEN_4TO6	= (1<<0),
+	CACHE_F_SEEN_6TO4	= (1<<1),
+	CACHE_F_GEN_IDENT	= (1<<2),
+	CACHE_F_REP_AGEOUT	= (1<<3),
+};
 
+/// Configuration structure
 struct config {
 	char tundev[IFNAMSIZ];
 	char data_dir[512];
@@ -242,7 +255,6 @@ struct config {
 	struct dynamic_pool *dynamic_pool;
 	int hash_bits;
 	int cache_size;
-	int allow_ident_gen;
 	int ipv6_offlink_mtu;
 	int lazy_frag_hdr;
 
@@ -266,10 +278,12 @@ struct config {
 	int wkpf_strict;
 };
 
-/* Tayga Packet Error Codes */
-#define ERROR_NONE 0
-#define ERROR_REJECT -1
-#define ERROR_DROP -2
+/// Packet error codes
+enum {
+	ERROR_NONE = 0,
+	ERROR_REJECT = -1,
+	ERROR_DROP = -2,
+};
 
 
 /* Macros and static functions */

.gitignore
@@ -11,3 +11,8 @@ tayga-cov
 *.pcap
 *.pyc
 *.log
+html/*
+latex/*
+version.h
+.vscode/*
+callgrind*

Makefile
@@ -1,7 +1,7 @@
 # Simple Makefile generated based on makefile.am
 
 CC := gcc
-CFLAGS := -Wall -O2 -Isrc
+CFLAGS := -Wall -O2
 LDFLAGS := 
 SOURCES := nat64.c addrmap.c dynamic.c tayga.c conffile.c
 TARGET := tayga
@@ -10,15 +10,21 @@ TARGET-COV := $(TARGET)-cov
 all: $(TARGET)
 cov: $(TARGET-COV)
 
+# Version generation
+version.h: .git/*
+	@echo "#define TAYGA_VERSION \"$(shell git describe --tags --abbrev=0)\"" > $@
+	@echo "#define TAYGA_COMMIT \"$(shell git rev-parse HEAD)\"" >> $@
+
 # Dependency generation
-DEPS := $(SOURCES:.c=.d)
-%.d: %.c
-	@$(CC) $(CFLAGS) -MM -MT $(@:.d=.o) $< > $@
+tayga.d: $(SOURCES) version.h Makefile
+	$(CC) $(CFLAGS) -MM $(SOURCES) -MT tayga $< > $@
 
--include $(DEPS)
+-include tayga.d
 
-$(TARGET): $(SOURCES)
-	$(CC) $(LDFLAGS) -o $@ $(SOURCES)
+
+
+$(TARGET): $(SOURCES) Makefile
+	$(CC) $(CFLAGS) -o $@ $(SOURCES) $(LDFLAGS) -flto
 
 $(TARGET-COV): $(TARGET)
 	$(CC) $(LDFLAGS) -o $@ $(SOURCES) -coverage -fcondition-coverage
@@ -27,7 +33,7 @@ cov-report:
 	gcov -a -g -f *.gcno
 
 clean:
-	rm -f $(TARGET) $(DEPS) $(TARGET-COV) *.gcda *.gcno
+	rm -f $(TARGET) tayga.d version.h $(TARGET-COV) *.gcda *.gcno
 
 install: $(TARGET)
 	# TODO

addrmap.c
@@ -21,6 +21,15 @@
 extern struct config *gcfg;
 extern time_t now;
 
+/**
+ * @brief Check if an IPv4 address is valid
+ * 
+ * Checks if an address is within the ranges which are reserved
+ * by protocol, such as multicast, link-local, etc.
+ *
+ * @param a struct in_addr address to validate
+ * @returns ERROR_DROP if invalid, else 0
+ */
 int validate_ip4_addr(const struct in_addr *a)
 {
 	/* First Octet == 0 */
@@ -47,6 +56,15 @@ int validate_ip4_addr(const struct in_addr *a)
 	return ERROR_NONE;
 }
 
+/**
+ * @brief Check if an IPv6 address is valid
+ *  
+ * Checks if an address is within the ranges which are reserved
+ * by protocol, such as multicast, link-local, etc.
+ *
+ * @param a struct in6_addr address to validate
+ * @returns ERROR_DROP if invalid, else 0
+ */
 int validate_ip6_addr(const struct in6_addr *a)
 {
 	/* Well-known prefix for NAT64, plus Local-Use Space */
@@ -69,6 +87,16 @@ int validate_ip6_addr(const struct in6_addr *a)
 	return ERROR_NONE;
 }
 
+/**
+ * @brief Check if an IPv4 address is private
+ *  
+ * Checks if an address is within the ranges which are reserved
+ * by IANA, and therefore, must not be translated using the
+ * well-known prefix (64:ff9b::/96)
+ *
+ * @param a struct in_addr address to validate
+ * @returns ERROR_REJECT if invalid, else 0
+ */
 int is_private_ip4_addr(const struct in_addr *a)
 {
 	/* 10.0.0.0/8 RFC1918 */
@@ -106,16 +134,31 @@ int is_private_ip4_addr(const struct in_addr *a)
 	return ERROR_NONE;
 }
 
+/**
+ * @brief Calculate the mask of an IPv4 address
+ *
+ * @param mask Pointer to return mask
+ * @param addr Pointer to address
+ * @param len length in bits of IPv4 mask
+ * @returns -1 if address has host bits of mask set
+ */
 int calc_ip4_mask(struct in_addr *mask, const struct in_addr *addr, int len)
 {
 	mask->s_addr = htonl(~(0xffffffff >> len));
 	if (len == 32) mask->s_addr = 0xffffffff;
 	if (addr && (addr->s_addr & ~mask->s_addr))
-		return -1; //todo fix this error code
+		return -1;
 	return 0;
-
 }
 
+/**
+ * @brief Calculate the mask of an IPv6 address
+ *
+ * @param mask Pointer to return mask
+ * @param addr Pointer to address
+ * @param len length in bits of IPv6 mask
+ * @returns -1 if address has host bits of mask set
+ */
 int calc_ip6_mask(struct in6_addr *mask, const struct in6_addr *addr, int len)
 {
 	if (len > 32) {
@@ -148,7 +191,7 @@ int calc_ip6_mask(struct in6_addr *mask, const struct in6_addr *addr, int len)
 			(addr->s6_addr32[1] & ~mask->s6_addr32[1]) ||
 			(addr->s6_addr32[2] & ~mask->s6_addr32[2]) ||
 			(addr->s6_addr32[3] & ~mask->s6_addr32[3]))
-		return -1; //todo fix this error code
+		return -1;
 	return 0;
 }
 
@@ -161,15 +204,10 @@ static uint32_t hash_ip4(const struct in_addr *addr4)
 static uint32_t hash_ip6(const struct in6_addr *addr6)
 {
 	uint32_t h;
-
-	h = ((uint32_t)addr6->s6_addr16[0] + gcfg->rand[0]) *
-		((uint32_t)addr6->s6_addr16[1] + gcfg->rand[1]);
-	h ^= ((uint32_t)addr6->s6_addr16[2] + gcfg->rand[2]) *
-		((uint32_t)addr6->s6_addr16[3] + gcfg->rand[3]);
-	h ^= ((uint32_t)addr6->s6_addr16[4] + gcfg->rand[4]) *
-		((uint32_t)addr6->s6_addr16[5] + gcfg->rand[5]);
-	h ^= ((uint32_t)addr6->s6_addr16[6] + gcfg->rand[6]) *
-		((uint32_t)addr6->s6_addr16[7] + gcfg->rand[7]);
+	h = addr6->s6_addr32[0] + gcfg->rand[0];
+	h ^= addr6->s6_addr32[1] + gcfg->rand[1];
+	h ^= addr6->s6_addr32[2] + gcfg->rand[2];
+	h ^= addr6->s6_addr32[3] + gcfg->rand[3];
 	return h >> (32 - gcfg->hash_bits);
 }
 
@@ -180,6 +218,12 @@ static void add_to_hash_table(struct cache_entry *c, uint32_t hash4,
 	list_add(&c->hash6, &gcfg->hash_table6[hash6]);
 }
 
+/**
+ * @brief Initialize address translation cache
+ *  
+ * TODO Document how the cache works better
+ *
+ */
 void create_cache(void)
 {
 	int i, hash_size = 1 << gcfg->hash_bits;
@@ -243,7 +287,12 @@ static struct cache_entry *cache_insert(const struct in_addr *addr4,
 	add_to_hash_table(c, hash4, hash6);
 	return c;
 }
-
+/**
+ * @brief Check if an IPv4 address is in the cache
+ *  
+ * @param addr4 IPv4 address to check
+ * @returns Cache entry, or NULL if none found
+ */
 struct map4 *find_map4(const struct in_addr *addr4)
 {
 	struct list_head *entry;
@@ -256,7 +305,12 @@ struct map4 *find_map4(const struct in_addr *addr4)
 	}
 	return NULL;
 }
-
+/**
+ * @brief Check if an IPv6 address is in the cache
+ *  
+ * @param addr6 IPv6 address to check
+ * @returns Cache entry, or NULL if none found
+ */
 struct map6 *find_map6(const struct in6_addr *addr6)
 {
 	struct list_head *entry;
@@ -269,7 +323,13 @@ struct map6 *find_map6(const struct in6_addr *addr6)
 	}
 	return NULL;
 }
-
+/**
+ * @brief Insert an IPv4 entry into the cache
+ *  
+ * @param map4 Cache entry to add
+ * @param conflict Pointer to return conflicting object
+ * @returns -1 on conflict
+ */
 int insert_map4(struct map4 *m, struct map4 **conflict)
 {
 	struct list_head *entry;
@@ -291,7 +351,13 @@ conflict:
 		*conflict = s;
 	return -1;
 }
-
+/**
+ * @brief Insert an IPv6 entry into the cache
+ *  
+ * @param map6 Cache entry to add
+ * @param conflict Pointer to return conflicting object
+ * @returns -1 on conflict
+ */
 int insert_map6(struct map6 *m, struct map6 **conflict)
 {
 	struct list_head *entry, *insert_pos = NULL;
@@ -317,7 +383,15 @@ conflict:
 		*conflict = s;
 	return -1;
 }
-
+/**
+ * @brief Append an IPv4 address to an IPv6 translation prefix
+ *  
+ * @param[out] addr6 Return IPv6 address
+ * @param[in] addr4 IPv4 address
+ * @param[in] prefix IPv6 Prefix
+ * @param[in] prefix_len IPv6 prefix length (must be defined by RFC6052)
+ * @returns ERROR_DROP on invalid prefix
+ */
 int append_to_prefix(struct in6_addr *addr6, const struct in_addr *addr4,
 		const struct in6_addr *prefix, int prefix_len)
 {
@@ -405,6 +479,14 @@ int append_to_prefix(struct in6_addr *addr6, const struct in_addr *addr4,
 	}
 }
 
+/**
+ * @brief Map IPv4 to IPv6
+ *  
+ * @param[out] addr6 Return IPv6 address
+ * @param[in] addr4 IPv4 address
+ * @param[out] c_ptr Cache entry
+ * @returns ERROR_REJECT or ERROR_DROP on error
+ */
 int map_ip4_to_ip6(struct in6_addr *addr6, const struct in_addr *addr4,
 		struct cache_entry **c_ptr)
 {
@@ -553,7 +635,15 @@ static int extract_from_prefix(struct in_addr *addr4,
 	}
 	return validate_ip4_addr(addr4);
 }
-
+/**
+ * @brief Map IPv6 to IPv4
+ *  
+ * @param[out] addr4 Return IPv6 address
+ * @param[in] addr6 IPv4 address
+ * @param[out] c_ptr Cache entry
+ * @param[in] dyn_allow Allow dynamic allocation for this mapping
+ * @returns ERROR_REJECT or ERROR_DROP on error
+ */
 int map_ip6_to_ip4(struct in_addr *addr4, const struct in6_addr *addr6,
 		struct cache_entry **c_ptr, int dyn_alloc)
 {
@@ -586,7 +676,7 @@ int map_ip6_to_ip4(struct in_addr *addr4, const struct in6_addr *addr6,
 		if (dyn_alloc)
 			map6 = assign_dynamic(addr6);
 		if (!map6)
-			return -1;
+			return ERROR_REJECT; //TODO what's the right behavior here
 	}
 
 	switch (map6->type) {
@@ -654,6 +744,10 @@ static void report_ageout(struct cache_entry *c)
 	d->cache_entry = NULL;
 }
 
+/**
+ * @brief Perform periodic address cache maintenance
+ *  
+ */
 void addrmap_maint(void)
 {
 	struct list_head *entry, *next;

commit 04e39f74db104862764a91a508b11a688600526c

Make tayga buildable with gcc -Wextra (#131)
Used @Skyb0rg007's suggestion for (void)arg

* tayga.c: address Wsign-compare

* nat64.c: address Wsign-compare

The original litech.org source had often used `int` to store buffer
lengths for some unknown reason, paving the way for possible negative
offset bugs. We fix them all to use `uint32_t`, since the lengths we
deal with fit in 32 bits nicely, they are usually upper bounded by outer
IPv6 packet length.

cfg->ipv6_offlink_mtu could be a `uint16_t`, I decided not to fiddle
with struct field offsets and alignment for now.

* tayga.h: make MTU_ADJ explicitly unsigned

* xlate_header_6to4: address Wunused-parameter

Fixes: 8578d24f9669 ("Documentation & Work (#59)")

* conffile.c: address Wunused-parameter

We mark those as "maybe-unused" with a compiler attribute.

* Review changes

---------

Co-authored-by: apalrd <adventure@apalrd.net>

conffile.c
@@ -112,6 +112,9 @@ static void abort_on_conflict6(char *msg, int ln, struct map6 *old)
 
 static int config_ipv4_addr(int ln, int arg_count, char **args)
 {
+	//arg_count unused
+	(void)arg_count;
+
 	if (gcfg->local_addr4.s_addr) {
 		slog(LOG_CRIT, "Error: duplicate ipv4-addr directive on "
 				"line %d\n", ln);
@@ -136,6 +139,9 @@ static int config_ipv4_addr(int ln, int arg_count, char **args)
 
 static int config_ipv6_addr(int ln, int arg_count, char **args)
 {
+	//arg_count unused
+	(void)arg_count;
+
 	if (gcfg->local_addr6.s6_addr[0]) {
 		slog(LOG_CRIT, "Error: duplicate ipv6-addr directive on line "
 				"%d\n", ln);
@@ -156,6 +162,9 @@ static int config_ipv6_addr(int ln, int arg_count, char **args)
 
 static int config_prefix(int ln, int arg_count, char **args)
 {
+	//arg_count unused
+	(void)arg_count;
+
 	struct map_static *m;
 	struct map6 *m6;
 
@@ -199,6 +208,9 @@ static int config_prefix(int ln, int arg_count, char **args)
 
 static int config_wkpf_strict(int ln, int arg_count, char **args)
 {
+	//arg_count unused
+	(void)arg_count;
+
 	if (!strcasecmp(args[0], "true") ||
 	    !strcasecmp(args[0], "on") ||
 	    !strcasecmp(args[0], "yes") ||
@@ -218,6 +230,9 @@ static int config_wkpf_strict(int ln, int arg_count, char **args)
 
 static int config_udp_cksum_mode(int ln, int arg_count, char **args)
 {
+	//arg_count unused
+	(void)arg_count;
+
 	/* Drop, or some variant of that */
 	if (!strncasecmp(args[0], "dr",2)){
 		gcfg->udp_cksum_mode = UDP_CKSUM_DROP;
@@ -236,6 +251,9 @@ static int config_udp_cksum_mode(int ln, int arg_count, char **args)
 
 static int config_tun_device(int ln, int arg_count, char **args)
 {
+	//arg_count unused
+	(void)arg_count;
+
 	if (gcfg->tundev[0]) {
 		slog(LOG_CRIT, "Error: duplicate tun-device directive on line "
 				"%d\n", ln);
@@ -252,6 +270,9 @@ static int config_tun_device(int ln, int arg_count, char **args)
 
 static int config_map(int ln, int arg_count, char **args)
 {
+	//arg_count unused
+	(void)arg_count;
+
 	struct map_static *m;
 	struct map4 *m4;
 	struct map6 *m6;
@@ -324,6 +345,9 @@ static int config_map(int ln, int arg_count, char **args)
 
 static int config_dynamic_pool(int ln, int arg_count, char **args)
 {
+	//arg_count unused
+	(void)arg_count;
+
 	struct dynamic_pool *pool;
 	struct map4 *m4;
 
@@ -384,6 +408,9 @@ static int config_dynamic_pool(int ln, int arg_count, char **args)
 
 static int config_data_dir(int ln, int arg_count, char **args)
 {
+	//arg_count unused
+	(void)arg_count;
+
 	if (gcfg->data_dir[0]) {
 		slog(LOG_CRIT, "Error: duplicate data-dir directive on line "
 				"%d\n", ln);
@@ -399,6 +426,10 @@ static int config_data_dir(int ln, int arg_count, char **args)
 
 static int config_strict_fh(int ln, int arg_count, char **args)
 {
+	//unused
+	(void)arg_count;
+	(void)args;
+	 
 	slog(LOG_WARNING,"Warning: strict-frag-hdr deprecated on line %d\n",ln);
 	return ERROR_NONE;
 }
@@ -431,7 +462,10 @@ static int config_log(int ln, int arg_count, char **args)
 
 static int config_offlink_mtu(int ln, int arg_count, char **args)
 {
-	/* Offlink MTU alreadys et */
+	//arg_count unused
+	(void)arg_count;
+	
+	/* Offlink MTU already set? */
 	if (gcfg->ipv6_offlink_mtu) {
 		slog(LOG_CRIT, "Error: duplicate offlink-mtu directive on "
 				"line %d\n", ln);

nat64.c
@@ -127,7 +127,7 @@ static void log_pkt6(int err, struct pkt *p, const char *msg)
 		type, saddr, daddr, (p->header_len + p->data_len),p->data_proto,msg);
 }
 
-static uint16_t ip_checksum(void *d, int c)
+static uint16_t ip_checksum(void *d, uint32_t c)
 {
 	uint32_t sum = 0xffff;
 	uint16_t *p = d;
@@ -196,7 +196,7 @@ static uint16_t convert_cksum(struct ip6 *ip6, struct ip4 *ip4)
 
 static void host_send_icmp4(uint8_t tos, struct in_addr *src,
 		struct in_addr *dest, struct icmp *icmp,
-		uint8_t *data, int data_len)
+		uint8_t *data, uint32_t data_len)
 {
 	struct ip4_icmp header;
 	struct iovec iov[2];
@@ -231,7 +231,7 @@ static void host_send_icmp4_error(uint8_t type, uint8_t code, uint32_t word,
 		struct pkt *orig)
 {
 	struct icmp icmp;
-	int orig_len;
+	uint32_t orig_len;
 
 	/* Don't send ICMP errors in response to ICMP messages other than
 	   echo request */
@@ -350,7 +350,7 @@ static void xlate_4to6_data(struct pkt *p)
 	struct iovec iov[2];
 	int no_frag_hdr = 0;
 	uint16_t off = ntohs(p->ip4->flags_offset);
-	int frag_size;
+	uint32_t frag_size;
 	int ret;
 
 	frag_size = gcfg->ipv6_offlink_mtu;
@@ -765,7 +765,7 @@ void handle_ip4(struct pkt *p)
 
 static void host_send_icmp6(uint8_t tc, struct in6_addr *src,
 		struct in6_addr *dest, struct icmp *icmp,
-		uint8_t *data, int data_len)
+		uint8_t *data, uint32_t data_len)
 {
 	struct ip6_icmp header;
 	struct iovec iov[2];
@@ -797,7 +797,7 @@ static void host_send_icmp6_error(uint8_t type, uint8_t code, uint32_t word,
 				struct pkt *orig)
 {
 	struct icmp icmp;
-	int orig_len;
+	uint32_t orig_len;
 
 	/* Don't send ICMP errors in response to ICMP messages other than
 	   echo request */
@@ -836,7 +836,7 @@ static void host_handle_icmp6(struct pkt *p)
 }
 
 static void xlate_header_6to4(struct pkt *p, struct ip4 *ip4,
-		int payload_length, struct cache_entry *dest)
+		int payload_length)
 {
 	ip4->ver_ihl = 0x45;
 	ip4->tos = (ntohl(p->ip6->ver_tc_fl) >> 20) & 0xff;
@@ -975,7 +975,7 @@ static void xlate_6to4_data(struct pkt *p)
 		return;
 	}
 
-	xlate_header_6to4(p, &header.ip4, p->data_len, dest);
+	xlate_header_6to4(p, &header.ip4, p->data_len);
 	--header.ip4.ttl;
 
 	if (xlate_payload_6to4(p, &header.ip4,0) < 0)
@@ -1002,7 +1002,7 @@ static void xlate_6to4_data(struct pkt *p)
 
 static int parse_ip6(struct pkt *p,int em)
 {
-	int hdr_len;
+	uint32_t hdr_len;
 	uint8_t seg_left = 0;
 	uint16_t seg_ptr = sizeof(struct ip6);
 
@@ -1230,7 +1230,7 @@ static void xlate_6to4_icmp_error(struct pkt *p)
 	}
 
 	xlate_header_6to4(&p_em, &header.ip4_em,
-		ntohs(p_em.ip6->payload_length) - p_em.header_len, NULL);
+		ntohs(p_em.ip6->payload_length) - p_em.header_len);
 
 	header.ip4_em.cksum =
 		ip_checksum(&header.ip4_em, sizeof(header.ip4_em));
@@ -1249,7 +1249,7 @@ static void xlate_6to4_icmp_error(struct pkt *p)
 	}
 
 	xlate_header_6to4(p, &header.ip4, sizeof(header.icmp) +
-				sizeof(header.ip4_em) + p_em.data_len, NULL);
+				sizeof(header.ip4_em) + p_em.data_len);
 	--header.ip4.ttl;
 
 	header.ip4.cksum = ip_checksum(&header.ip4, sizeof(header.ip4));

tayga.c
@@ -363,12 +363,12 @@ static void read_from_tun(void)
 				"device: %s\n", strerror(errno));
 		return;
 	}
-	if (ret < sizeof(struct tun_pi)) {
+	if ((size_t)ret < sizeof(struct tun_pi)) {
 		slog(LOG_WARNING, "short read from tun device "
 				"(%d bytes)\n", ret);
 		return;
 	}
-	if (ret == gcfg->recv_buf_size) {
+	if ((uint32_t)ret == gcfg->recv_buf_size) {
 		slog(LOG_WARNING, "dropping oversized packet\n");
 		return;
 	}
@@ -691,7 +691,7 @@ int main(int argc, char **argv)
 
 	if (pidfile) {
 		snprintf(addrbuf, sizeof(addrbuf), "%ld\n", (long)getpid());
-		if (write(pidfd, addrbuf, strlen(addrbuf)) != strlen(addrbuf)) {
+		if (write(pidfd, addrbuf, strlen(addrbuf)) != (ssize_t)strlen(addrbuf)) {
 			slog(LOG_CRIT, "Error, unable to write PID file.\n");
 			exit(1);
 		}

tayga.h
@@ -169,7 +169,7 @@ static_assert(sizeof(struct icmp) == 8,"Struct ICMP must be 8 bytes long");
 
    (See http://www.ietf.org/mail-archive/web/behave/current/msg08499.html)
  */
-#define MTU_ADJ		20
+#define MTU_ADJ		20u
 
 /* Minimum MTU allowed by IPv6 */
 #define MTU_MIN 1280
@@ -296,7 +296,7 @@ struct config {
 	struct dynamic_pool *dynamic_pool;
 	int hash_bits;
 	int cache_size;
-	int ipv6_offlink_mtu;
+	uint32_t ipv6_offlink_mtu;
 
 	int urandom_fd;
 	int tun_fd;