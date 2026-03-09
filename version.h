4494b76b21c2010b1f45d889e2f3986edaf7667f
apalrd <adventure@apalrd.net>
2026-03-09 23:34:06 +0000

    fix version.h missing

---
+++ version.h
@@ -0,0 +1,8 @@
+#ifndef __TAYGA_VERSION_H__
+#define __TAYGA_VERSION_H__
+
+#define TAYGA_VERSION "0.9.6-25-g46e283e"
+#define TAYGA_BRANCH  "heads/dev-mapfile"
+#define TAYGA_COMMIT  "46e283e7c7d0a7016990fdc1981f590bf40900f1"
+
+#endif /* #ifndef __TAYGA_VERSION_H__ */
